0d7b6cdab7b17c7e68244be453ec924bd3ae36a1
wpxon <wpxon7@gmail.com>
2022-02-28 14:29:10 +0600

    Initialization admin post type and metabox to the main plugin file

--- before-after-image-comparison-slider.php
+++ before-after-image-comparison-slider.php
@@ -13,4 +13,67 @@
  */
 
 defined( 'ABSPATH' ) || exit;
- 
+
+require_once __DIR__ . '/vendor/autoload.php';
+// Initialize post type.
+new BAICS\Post_Type();
+// Initialize admin setigns
+new BAICS\Admin();
+
+wpx_metabox(
+    array(
+        'meta_box_id'   =>  'sssample_meta_id',
+        'label'         =>  __( 'Sample Meta Box 2' ),
+        'post_type'     =>  array( 'baics' ),
+        'context'       =>  'normal', // side|normal|advanced
+        'priority'      =>  'high', // high|low
+        'hook_priority' =>  10,
+        'tab'           =>  true,
+        'tabs' => array(
+            array(
+                'id'    => 'tab_1', 
+                'title' => 'Tab 1',
+                'fields' => array(
+                    array(
+                        'name'      =>  't1',
+                        'label'     =>  __( 'Tab 1' ),
+                        'type'      =>  'text',
+                        'desc'      =>  __( 'This is a text field.' ),
+                        'class'     =>  'title',
+                        'default'   =>  'Hello World!',
+                        'readonly'  =>  false,
+                        'disabled'  =>  false,
+                    ) 
+                ),
+            ),
+            array(
+                'id'    => 'tab_2', 
+                'title' => 'Tab 2',
+                'fields' => array(
+                    array(
+                        'name'      =>  'te',
+                        'label'     =>  __( 'Tab 2' ),
+                        'type'      =>  'text',
+                        'desc'      =>  __( 'This is a text field.' ),
+                        'class'     =>  'title',
+                        'default'   =>  'Hello World!',
+                        'readonly'  =>  false,
+                        'disabled'  =>  false,
+                    ) 
+                ),
+            ),
+        ),
+        'fields'        =>  array( 
+            array(
+                'name'      =>  'sssample_text',
+                'label'     =>  __( 'Text Field 2' ),
+                'type'      =>  'text',
+                'desc'      =>  __( 'This is a text field.' ),
+                'class'     =>  'title',
+                'default'   =>  'Hello World!',
+                'readonly'  =>  false,
+                'disabled'  =>  false,
+            ) 
+        )
+    )
+);
