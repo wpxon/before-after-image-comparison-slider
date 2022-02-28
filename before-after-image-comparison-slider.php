f249d28ab7d0e89afee7e40f9bbcc670b7683d25
wpxon <wpxon7@gmail.com>
2022-02-28 14:25:07 +0600

    Initialzation

---
+++ before-after-image-comparison-slider.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin name: Before After Image Comparison Slider for WordPress
+ * Plugin URI: http://wpxon.com/plugins/before-after-image-compariosn-slider
+ * Description: A image comparison plugin for WordPress.
+ * Version: 1.0.0
+ * Author: WPxon
+ * Author URI: http://wpxon.com
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: baics
+ * Domain Path: /languages
+ */
+
+defined( 'ABSPATH' ) || exit;
+ 
