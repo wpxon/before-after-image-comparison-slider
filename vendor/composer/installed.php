5dbe7a822b9dca6fb05e669c2df07165dabbcdf1
wpxon <wpxon7@gmail.com>
2022-02-28 14:29:51 +0600

    Ran composer update to get missing vendor file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2b084dbe5e830b76da5eb355b75f9e1d478738b2',
+        'name' => 'wpxon/before-after-image-comparison-slider',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpxon/before-after-image-comparison-slider' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2b084dbe5e830b76da5eb355b75f9e1d478738b2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
